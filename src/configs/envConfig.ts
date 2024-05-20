565067077a2f8b66290b77f934cc85d4b17dc3f1
quangtm-amcolab <quangtm@amcolab.vn>
2024-05-20 14:10:20 +0700

    config env

---
+++ src/configs/envConfig.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod';
+
+const configSchema = z.object({
+  VITE_BACKEND_BASE_URL: z.string(),
+});
+// Phương thức "safeParse" được sử dụng để phân tích đối tượng nhập vào dựa trên schema đã định.
+// Nếu có lỗi, phương thức này sẽ không ném ra ngoại lệ mà trả về một đối tượng chứa thông tin lỗi.
+const configProject = configSchema.safeParse({
+  VITE_BACKEND_BASE_URL: import.meta.env.VITE_BACKEND_BASE_URL,
+});
+if (!configProject.success) {
+  console.error(configProject.error.issues);
+  throw new Error('Các giá trị khai báo trong file .env không hợp lệ');
+}
+
+const envConfig = configProject.data;
+export default envConfig;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import envConfig from 'src/configs/envConfig';
 
 const Home = () => {
+  console.log('Home', envConfig.VITE_BACKEND_BASE_URL);
   return <div>Home</div>;
 };
 
